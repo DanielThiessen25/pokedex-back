743b245b2c9041774d97ec5078f0ddbcc0ab5435
Daniel Luiz <danielluizthiessen@gmail.com>
2021-08-07 20:52:39 -0300

    Preparing for deploy

--- src/controllers/userConroller.ts
+++ src/controllers/userConroller.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express";
 
 import * as userService from "../services/userService";
 
-export async function getUsers (req: Request, res: Response) {
+export async function createUser (req: Request, res: Response) {
   try {
-    const users = await userService.getUsers();
+    const users = await userService.createUser(req.body);
     res.send(users);
   } catch (err) {
     console.error(err);
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
+
 import { getRepository } from "typeorm";
+import bcrypt from "bcrypt";
 
 import User from "../entities/User";
 
-export async function getUsers () {
-  const users = await getRepository(User).find({
-    select: ["id", "email"]
-  });
-  
-  return users;
+interface UserCreate {
+  email: string;
+  password:string;
+}
+
+export async function createUser (user: UserCreate) {
+  const hashPassword = bcrypt.hashSync(user.password, 10);
+  const finalUser : UserCreate = {email:user.email, password:hashPassword};
+  const newUser = await getRepository(User).create(finalUser);
+  await getRepository(User).save(newUser);
+  return newUser;
 }
