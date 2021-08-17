fc6e1691965bc0297b15ec70b8ddaf9af11ddaf0
ronald <ronaldjunior2@hotmail.com>
2021-08-17 15:58:36 -0300

    preparando review

--- tests/utils/database.ts
+++
@@ -1,9 +0,0 @@
-import { getRepository } from "typeorm";
-
-import User from "../../src/entities/User";
-import Pokemon from "../../src/entities/Pokemon";
-
-export async function clearDatabase () {
-  await getRepository(User).delete({});
-  await getRepository(Pokemon).delete({});
-}
